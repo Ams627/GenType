4ce9cbab47bd8dd77c83b678b4de93a59ee2f88c
Adrian Sims <adriansims62781@gmail.com>
2021-03-16 15:22:14 +0100

    Add project files.

---
+++ GenType/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace GenType
+{
+    /// <summary>
+    /// Converts a data type to another data type.
+    /// </summary>
+    public static class Cast
+    {
+        /// <summary>
+        /// Converts input to Type of default value or given as typeparam T
+        /// </summary>
+        /// <typeparam name="T">typeparam is the type in which value will be returned, it could be any type eg. int, string, bool, decimal etc.</typeparam>
+        /// <param name="input">Input that need to be converted to specified type</param>
+        /// <param name="defaultValue">defaultValue will be returned in case of value is null or any exception occures</param>
+        /// <returns>Input is converted in Type of default value or given as typeparam T and returned</returns>
+        private static T To<T>(object input, T defaultValue)
+        {
+            var result = defaultValue;
+            try
+            {
+                if (input == null || input == DBNull.Value) return result;
+                if (typeof(T).IsEnum)
+                {
+                    result = (T)Enum.ToObject(typeof(T), To(input, Convert.ToInt32(defaultValue)));
+                }
+                else
+                {
+                    result = (T)Convert.ChangeType(input, typeof(T));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"error: {ex}");
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Converts input to Type of typeparam T
+        /// </summary>
+        /// <typeparam name="T">typeparam is the type in which value will be returned, it could be any type eg. int, string, bool, decimal etc.</typeparam>
+        /// <param name="input">Input that need to be converted to specified type</param>
+        /// <returns>Input is converted in Type of default value or given as typeparam T and returned</returns>
+        public static T To<T>(object input)
+        {
+            return To(input, default(T));
+        }
+
+
+
+    }
+
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var parse1 = TypeHelper.GetParser<int>("double");
+                var res = parse1.Invoke("12345.4");
+                Console.WriteLine();
+                //var method = typeof(Cast).GetMethod("To").MakeGenericMethod(t1);
+                //var res = method.Invoke(null, new object[] { "1234" });
+                //Console.WriteLine();
+                //Type[] typeArgs = { Type.GetType("TypeRepository") };
+                ////Type repositoryType = genericType.MakeGenericType(typeArgs);
+                //var param = new System.Data.SqlClient.SqlParameter();
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+    }
+}
---
+++ GenType/TypeHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace GenType
+{
+    public class TypeHelper
+    {
+        // types like int are C# aliases for CLR types - there's no 
+        // method in C# to convert C# alias name (as a string) into a type, so
+        // we have to provide a lookup:
+        private static readonly Dictionary<string, Type> csharpAliasToType =
+            new Dictionary<string, Type> {
+                { "byte",     typeof(byte)      },
+                { "short",    typeof(short)     },
+                { "int",      typeof(int)       },
+                { "uint",     typeof(uint)      },
+                { "long",     typeof(long)      },
+                { "float",    typeof(float)     },
+                { "double",   typeof(double)    },
+                { "decimal",  typeof(decimal)   },
+                { "char",     typeof(char)      },
+                { "string",   typeof(string)    },
+            };
+
+        public static Type GetTypeFromString(string typename)
+        {
+            if (!csharpAliasToType.TryGetValue(typename, out var type))
+            {
+                return null;
+            }
+            return type;
+        }
+
+
+        /// <summary>
+        /// Return the parse function for the type as Func{string,obj}
+        /// </summary>
+        /// <param name="typename">the C# alias name of the type</param>
+        /// <returns>The parse function as a Func{string, obj}</returns>
+        public static Func<string, object> GetParser(string typename)
+        {
+            if (!csharpAliasToType.TryGetValue(typename, out var type))
+            {
+                return null;
+            }
+            var parseMethod = type.GetMethods().Where(x => x.Name == "Parse" && x.GetParameters().Count() == 1).First();
+            return FunctionTypeConverter<string>(parseMethod);
+        }
+
+
+        /// <summary>
+        /// Converts a function that takes a the generic type and returns a specific type into a function that takes
+        /// a string and returns an object. E.g. int.Parse is Func{string, int} and is converted to Func{string, object}
+        /// </summary>
+        /// <typeparam name="T">type of the function's parameter</typeparam>
+        /// <param name="method">MethodInfo of the function</param>
+        /// <returns>a Func which can be executed via Invoke</returns>
+        private static Func<T, object> FunctionTypeConverter<T>(MethodInfo method)
+        {
+            var parameter = method.GetParameters().Single();
+            var argument = Expression.Parameter(typeof(object), "argument");
+
+            var methodCall = Expression.Call(null, method, Expression.Convert(argument, parameter.ParameterType));
+
+            return Expression.Lambda<Func<T,object>>(
+            Expression.Convert(methodCall, typeof(object)), argument).Compile();
+        }
+
+    }
+}
